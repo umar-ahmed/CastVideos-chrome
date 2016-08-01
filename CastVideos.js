ef7d4d6c8c6cab8abb41952d0e1d5b6b7250a137
Umar Ahmed <umar.ahmed1998@gmail.com>
2016-07-31 20:19:04 -0400

    Update CastVideos.js

--- CastVideos.js
+++ CastVideos.js
@@ -142,7 +142,7 @@ CastPlayer.prototype.initializeCastPlayer = function() {
   // default set to the default media receiver app ID
   // optional: you may change it to point to your own
   //var applicationID = chrome.cast.media.DEFAULT_MEDIA_RECEIVER_APP_ID;
-  var applicationID = '4F8B3483';
+  var applicationID = '1F0C48B4';
 
   // auto join policy can be one of the following three
   var autoJoinPolicy = chrome.cast.AutoJoinPolicy.ORIGIN_SCOPED;
